a8791892a7f3fda92e37f765856da3fe5449fd8d
Orlando Santos <Santos.Orlando17@gmail.com>
2024-01-12 00:38:26 -0800

    lqy out content

--- src/Content.jsx
+++ src/Content.jsx
@@ -2,21 +2,16 @@ import "./Content.css"
 
 export function Content() {
   return (
-    <div className = "background">
-      <div className = "container header">
-        <div className="row justify-content-between">
-          <div className="col-md-6 logo">
-            <img className="ChurroLogo" src="src/images/ChurroLogo.png" width="350" height="110"></img>
+    <div className="info">
+      <div className="text-centder">
+        <div className="row">
+          <div className="aboutMe col-md-4 ">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, itaque cum? Delectus ut quidem expedita fugit neque sapiente natus eum et alias ipsam ea, quaerat voluptatibus quia dolorem facilis sunt!</p>
           </div>
-          <div className="col">
-            <img className="Churro" src="src/images/Churro.png" width="120" height="80"></img>
-          </div>
-          <div className="col">
-            <img className="Chocolate" src="src/images/Chocolate.png" width="120" height="80"></img>
-          </div>
-          <div className="col">
-            <img className="Cafe" src="src/images/Cafe.png" width="80" height="80"></img>
-          </div>
+          <div className="map col-md-4 offset-md-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto numquam, obcaecati quibusdam ipsam saepe cumque, amet ea minus quisquam eius debitis dicta velit inventore sit esse ut dolorem molestias dolorum?</div>
+        </div>
+        <div className="row">
+          <div className="contactMe col-md-6 offset-md-3">Ldddddorem ipsum dolor sit amet consectetur adipisicing elit. Quos, fuga beatae officia dolores commodi itaque nobis voluptatibus aspernatur nostrum inventore placeat aliquam vel dicta deserunt corporis eius aliquid repudiandae labore.</div>
         </div>
       </div>
     </div>
